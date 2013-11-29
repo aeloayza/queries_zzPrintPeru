51d901dcdcf6bbba35bb3672997eabb4f7627b85
aeloayza <aeloayza@gmail.com>
2013-11-28 23:01:39 -0500

    another query

--- queries.sql
+++ queries.sql
@@ -4,3 +4,10 @@ select order_date,sum(amount) from c3rn2_phonecase_orders where order_date betwe
 /* Query that shows sales per AID (Affiliate ID) */
 select id, order_no, order_date, amount from c3rn2_phonecase_orders where aid=1484;
 
+/* Query show number of sales per product (filter by date range) (group by: day, week, month, or year)*/
+/*week*/
+select week(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on (O.oid=PO.id)  where O.case_id='3' group by week(PO.order_date);
+/*month*/
+select month(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on (O.oid=PO.id)  where O.case_id='3' group by month(PO.order_date);
+/*year*/
+select year(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on (O.oid=PO.id)  where O.case_id='3' group by year(PO.order_date);
