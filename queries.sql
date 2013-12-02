d63c60cb7bc86eb48a171c84f7cb7e5c61b08244
gabriela <gzunigar@gmail.com>
2013-12-02 11:36:37 -0500

    device query

--- queries.sql
+++ queries.sql
@@ -11,3 +11,9 @@ select week(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phone
 select month(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on (O.oid=PO.id)  where O.case_id='3' group by month(PO.order_date);
 /*year*/
 select year(PO.order_date), O.case_id, O.case_name, sum(O.qty) from  c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on (O.oid=PO.id)  where O.case_id='3' group by year(PO.order_date);
+/* Query show number of sales per device (phone model)(filter by date range) (group by: day, week, month, or year)*/
+/*week*/
+Select week(PO.order_date), D.id, D.device_name, sum(O.qty) from  ((c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on O.oid=PO.id) inner join (c3rn2_phonecase_cases as C inner join c3rn2_phonecase_devices D on (C.device_id=D.id)) on (O.case_id=C.id)) group by D.id, week(PO.order_date);
+/*month*/
+Select month(PO.order_date), D.id, D.device_name, sum(O.qty) from  ((c3rn2_phonecase_order_items as O inner join c3rn2_phonecase_orders as PO on O.oid=PO.id) inner join (c3rn2_phonecase_cases as C inner join c3rn2_phonecase_devices D on (C.device_id=D.id)) on (O.case_id=C.id)) group by D.id, month(PO.order_date);
+
